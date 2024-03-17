5e96358b208d06b5c008d46ebc885a1e40b09667
JerryIs-strong <heyajerry@gmail.com>
2024-03-17 20:28:28 +0800

    Fix bug

--- src/plugins/private/json-fetcher.js
+++ src/plugins/private/json-fetcher.js
@@ -1,7 +1,12 @@
 fetch('/setting.json')
     .then(response => response.json())
     .then(data => {
-        localStorage.setItem('setting', JSON.stringify(data));
+        if(localStorage.getItem('setting') == "" || localStorage.getItem('setting') == null){
+            localStorage.setItem('setting', JSON.stringify(data));
+            window.location.reload();
+        }else{
+            localStorage.setItem('setting', JSON.stringify(data));
+        }
     })
     .catch(error => {
         console.error('Error fetching or parsing the setting.json file:', error);
